b24142599648d1b33c6a4ded08886e02271495be
Winterson <u19cse1028@cit.ac.in>
2024-08-16 20:29:11 +0530

    format

--- src/configs/Env.ts
+++ src/configs/Env.ts
@@ -1,5 +1,5 @@
-import { config } from "dotenv";
 import path from "node:path";
+import { config } from "dotenv";
 
 config({
 	path: path.join(__dirname, `../../.env.${process.env.NODE_ENV} || "dev"}`),
