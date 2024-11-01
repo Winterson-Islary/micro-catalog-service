a08213bc29e8206973e8033ce333227a127e2090
Winterson <u19cse1028@cit.ac.in>
2024-11-01 17:31:23 +0530

    add: global error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import config from "config";
 import express, { type Request, type Response } from "express";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
 	res.json({ message: `listening on port: ${config.get("server.port")}` });
 });
+
+app.use(globalErrorHandler);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import config from "config";
 import app from "./app";
+import logger from "./config/logger";
 
 function Server() {
 	const PORT: number = config.get("server.port") || 5502;
 	try {
 		app.listen(PORT, () => console.log("SERVER ACTIVE"));
-	} catch (err) {}
+	} catch (err) {
+		logger.error(`failed to initialize server; ${err}`);
+	}
 }
 
 Server();
