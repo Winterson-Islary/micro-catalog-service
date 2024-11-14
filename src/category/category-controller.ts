059ad5b8340ee2f12e59940f3ae379d4554fcac5
Winterson <u19cse1028@cit.ac.in>
2024-11-14 19:33:46 +0530

    add: category-controller

--- src/category/category-controller.ts
+++ src/category/category-controller.ts
@@ -1,7 +1,23 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import type { Logger } from "winston";
+import type { CategoryTypes, TCategoryService } from "./types";
 
 export class CategoryController {
-	async create(req: Request, res: Response) {
+	constructor(
+		private categoryService: TCategoryService,
+		private logger: Logger,
+	) {}
+	async create(req: Request, res: Response, next: NextFunction) {
+		if (!req.body.categoryInput) {
+			return next(
+				createHttpError(
+					400,
+					"{message: 'could not get valid input', location: 'category-controller'}",
+				),
+			);
+		}
+		this.categoryService.create(req.body.categoryInput);
 		res.json({});
 	}
 }
