2daca310bac2bb18c7afbb4c6f6896878a7ed95d
Winterson <u19cse1028@cit.ac.in>
2024-11-14 19:34:19 +0530

    modify: category-router file

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -1,13 +1,24 @@
-import { type Request, type Response, Router } from "Express";
+import {
+	type NextFunction,
+	type Request,
+	type Response,
+	Router,
+} from "Express";
+import logger from "../config/logger";
 import { CategoryController } from "./category-controller";
+import { CategoryService } from "./category-service";
 import { CategoryValidator } from "./category-validator";
 
 const router = Router();
+const categoryService = new CategoryService(logger);
+const categoryController = new CategoryController(categoryService, logger);
 
-const categoryController = new CategoryController();
-
-router.post("/", CategoryValidator, (req: Request, res: Response) => {
-	categoryController.create(req, res);
-});
+router.post(
+	"/",
+	CategoryValidator,
+	(req: Request, res: Response, next: NextFunction) => {
+		categoryController.create(req, res, next);
+	},
+);
 
 export default router;
