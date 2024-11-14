f5f50c3947ccad82b3101a4f56ce1b0f0a866191
Winterson <u19cse1028@cit.ac.in>
2024-11-14 19:35:13 +0530

    modify: app.ts file and category-validator.ts

--- src/category/category-validator.ts
+++ src/category/category-validator.ts
@@ -1,25 +1,8 @@
 import type { NextFunction, Request, Response } from "express";
-import createHttpError, { type HttpError } from "http-errors";
+import createHttpError from "http-errors";
 import { z } from "zod";
 import logger from "../config/logger";
-
-const ValidPriceType = ["base", "additional"] as const;
-const ValidWidgetType = ["switch", "radio"] as const;
-const CategoryValidatorSchema = z.object({
-	name: z.string(),
-	priceConfiguration: z.record(
-		z.object({
-			priceType: z.enum(ValidPriceType),
-			availableOptions: z.array(z.string()),
-		}),
-	),
-	attributes: z.object({
-		name: z.string(),
-		widgetType: z.enum(ValidWidgetType),
-		defaultValue: z.any(),
-		availableOptions: z.array(z.string()),
-	}),
-});
+import { type CategoryTypes, CategoryValidatorSchema } from "./types";
 
 export const CategoryValidator = async (
 	req: Request,
@@ -31,16 +14,14 @@ export const CategoryValidator = async (
 		const parsedPriceConfiguration =
 			req.body.priceConfiguration || undefined;
 		const parsedAttributes = req.body.attributes || undefined;
-		const tempData: z.infer<typeof CategoryValidatorSchema> = {
+		const tempData: CategoryTypes = {
 			name: parsedName,
 			priceConfiguration: parsedPriceConfiguration,
 			attributes: parsedAttributes,
 		};
-		const validatedInput =
+		const categoryInput =
 			await CategoryValidatorSchema.parseAsync(tempData);
-		req.body.name = validatedInput.name;
-		req.body.priceConfiguration = validatedInput.priceConfiguration;
-		req.body.attributes = validatedInput.attributes;
+		req.body.categoryInput = categoryInput;
 		return next();
 	} catch (error: unknown) {
 		if (error instanceof z.ZodError) {
