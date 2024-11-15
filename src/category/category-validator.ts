8db024d110e8c2955544e60d54f78f27cf294a35
Winterson <u19cse1028@cit.ac.in>
2024-11-16 04:16:25 +0530

    add: deleteCategory validator

--- src/category/category-validator.ts
+++ src/category/category-validator.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import { Types } from "mongoose";
 import { z } from "zod";
 import logger from "../config/logger";
 import {
@@ -72,3 +73,35 @@ export const PartialCategoryValidator = async (
 		next(createHttpError(400, "failed to parse category input"));
 	}
 };
+
+export const DeleteCategoryValidator = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const items: Types.ObjectId[] | undefined =
+			req.body.deleteItems || undefined;
+		if (!isArrayOfObjectIds(items)) {
+			throw new Error(
+				"{message: 'invalid ids passed for deletion', location: 'category-validator' }",
+			);
+		}
+		req.body.deleteItems = items;
+		return next();
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			next(createHttpError(400, error.message));
+		}
+		next(createHttpError(400, "failed to delete items"));
+	}
+};
+
+//! HELPER FUNCs
+function isObjectId(value: unknown): value is Types.ObjectId {
+	const id = value as string;
+	return Types.ObjectId.isValid(id);
+}
+function isArrayOfObjectIds(value: unknown): value is Array<Types.ObjectId> {
+	return Array.isArray(value) && value.every(isObjectId);
+}
