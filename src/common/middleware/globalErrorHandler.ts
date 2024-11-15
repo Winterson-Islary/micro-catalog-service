5690edd3a3e7caefd69784d5990593e547a2f160
Winterson <u19cse1028@cit.ac.in>
2024-11-16 04:15:58 +0530

    fix: res.status not a function by specifying res to be Response Object and not some func

--- src/common/middleware/globalErrorHandler.ts
+++ src/common/middleware/globalErrorHandler.ts
@@ -1,24 +1,27 @@
 import config from "config";
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import type { HttpError } from "http-errors";
 import { v4 as uuidv4 } from "uuid";
 import logger from "../../config/logger";
 
-function globalErrorHandler(err: HttpError, req: Request, res: Response) {
+function globalErrorHandler(
+	err: HttpError,
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
 	const errorId = uuidv4();
 	const statusCode = err.status || 500;
 	const isProduction = config.get("server.env") === "production";
 	const message = isProduction
 		? "An unexpected error has occurred."
 		: err.message;
-
 	logger.error(err.message, {
 		id: errorId,
 		error: err.stack,
 		path: req.path,
 		method: req.method,
 	});
-
 	res.status(statusCode).json({
 		errors: [
 			{
