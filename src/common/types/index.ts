9be35c62c4a7191ca2d0b70c9e034d44532752e5
Winterson <u19cse1028@cit.ac.in>
2024-11-23 04:25:26 +0530

    add: RequestAuth type

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,3 +1,11 @@
+import type { Request } from "express";
+
 export type AuthCookie = {
 	ACCESS_TOKEN: string;
 };
+export interface RequestAuth extends Request {
+	auth: {
+		sub: string;
+		role: string;
+	};
+}
