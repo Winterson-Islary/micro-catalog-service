81dfbc2426f8f8a817bec842130ab75594627cf8
Winterson <u19cse1028@cit.ac.in>
2025-01-02 19:57:42 +0530

    add tenantId to token payload

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -3,9 +3,18 @@ import type { Request } from "express";
 export type AuthCookie = {
 	ACCESS_TOKEN: string;
 };
+
 export interface RequestAuth extends Request {
 	auth: {
 		sub: string;
 		role: string;
+		tenantId: string;
 	};
 }
+
+export const Roles = {
+	CUSTOMER: "customer",
+	ADMIN: "admin",
+	MANAGER: "manager",
+	SUPERADMIN: "super",
+} as const;
