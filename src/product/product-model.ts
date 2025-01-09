cf748ff97681c95cb14aec29139cc154aaa8d067
Winterson <u19cse1028@cit.ac.in>
2025-01-09 23:51:00 +0530

    register mongoose plugin

--- src/product/product-model.ts
+++ src/product/product-model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const priceConfigurationSchema = new mongoose.Schema({
 	priceType: {
@@ -56,4 +57,5 @@ const productSchema = new mongoose.Schema(
 	{ timestamps: true },
 );
 
+productSchema.plugin(mongooseAggregatePaginate);
 export default mongoose.model("Product", productSchema);
