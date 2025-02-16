6a42d13005b57d04960c3319e7987785a97cead7
Winterson <u19cse1028@cit.ac.in>
2025-02-16 17:04:16 +0530

    fix: function return type to non-async

--- src/common/types/buckets.ts
+++ src/common/types/buckets.ts
@@ -9,5 +9,5 @@ export type TUploadReturn = {
 export interface FileStorage {
 	upload(data: ImageData): Promise<TUploadReturn>;
 	delete(filename: string): Promise<void>;
-	getObjectUrl(filename: string): Promise<string>;
+	getObjectUrl(filename: string): string;
 }
